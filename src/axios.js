684927152853a4d039c5c6c5226999c676b52f67
gGuiwolff <74604286+gGuiwolff@users.noreply.github.com>
2021-07-13 13:36:50 -0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 var instance = axios.create({
+    withCredentials: true,
     baseURL: "https://veg2.herokuapp.com",
     xsrfCookieName: "mytoken",
     xsrfHeaderName: "csrf-token",
